15e8ca180ceab084e1ac9d2172039556f2ba117d
hongyue-qiu <994059641@qq.com>
2020-11-04 23:54:31 +0800

    [邱鸿玥] feat: 完成problem1

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,13 @@
 package com.thoughtworks.capability.gtb.vo;
 
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+import com.thoughtworks.capability.gtb.DataSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +20,8 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
+  @JsonSerialize(using = DataSerializer.class)
   private Date time;
+  @JsonUnwrapped
   private UserVo user;
 }
